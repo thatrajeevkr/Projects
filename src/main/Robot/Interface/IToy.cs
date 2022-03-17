19a637f0c4753080dafbd7c4b2426bbf862c91fb
sc21rkr <sc21rkr@leeds.ac.uk>
2022-03-17 17:27:06 +0000

    Place Robot implementation

--- src/main/Robot/Interface/IToy.cs
+++ src/main/Robot/Interface/IToy.cs
@@ -1,7 +1,10 @@
+using Game.PlayBoard;
 namespace Game.Robot.Interface
 {
     public interface IToy
     {
-
+        public Position Position { get; set; }
+        public string Direction { get; set; }
+        void Place(Position position, string direction);
     }
 }
